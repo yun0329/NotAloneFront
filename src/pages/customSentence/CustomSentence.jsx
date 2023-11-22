7305493d8152659fa97f6b1dbef9189fa4fe7f6d
leeeemj <leemj2944@gmail.com>
2023-11-22 19:55:37 +0900

    feat: CustomSentence 컴포넌트 추가

--- src/pages/customSentence/CustomSentence.jsx
+++ src/pages/customSentence/CustomSentence.jsx
@@ -1,6 +1,6 @@
 import ListBox from "../../components/listBox/ListBox";
 import Toggle from "../../components/toggle/Toggle";
-// import Loading from "../../components/loading/Loading";
+import Loading from "../../components/loading/Loading";
 
 //category페이지에서 props 받아와서 ListBox에 넘겨주기 
 function CustomSentence(props){
@@ -8,7 +8,7 @@ function CustomSentence(props){
         <>
         <Toggle/>
         <ListBox gender="false" category="혼자살기"/>
-        {/* <Loading/> */}
+        <Loading/>
         </>
     )
 }
