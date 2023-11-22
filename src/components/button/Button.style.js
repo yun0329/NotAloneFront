1a1886cf54c000e2de15d7951af0be361a3b96fc
Whatdoyumin <lym4963@naver.com>
2023-11-22 22:49:50 +0900

    style: Button 컴포넌트 스타일 코드 구분

---
+++ src/components/button/Button.style.js
@@ -0,0 +1,13 @@
+import { styled } from 'styled-components';
+
+const CustomButton = styled.button`
+  width: 100%;
+  height: 50px;
+  padding: 14px;
+  border-radius: 18px;
+  font-size: 18px;
+  font-weight: 700;
+  background-color: #F7E1B9;
+`;
+
+export default CustomButton;
