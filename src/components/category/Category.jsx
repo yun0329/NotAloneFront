ae6390327df5cb89ac625ce60cd47eeb2f9d416d
Whatdoyumin <lym4963@naver.com>
2023-11-22 22:50:06 +0900

    style: Category 컴포넌트 스타일 코드 구분

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -1,29 +1,11 @@
 import React from 'react';
-
+import { CategoryStyle, TextStyle } from './Category.style';
 
 const Category = ({ text }) => {
-  const categoryStyle = {
-    width: '324px',
-    height: '149px',
-    backgroundColor: '#FEA900',
-    margin: '20px', 
-    borderRadius: '20px',
-    textAlign: 'center',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-  };
-
-  const textStyle = {
-    color: '#000000',
-    fontFamily: "'Do Hyeon', sans-serif",
-    fontSize: '32px',
-  };
-
   return (
-    <div style={categoryStyle}>
-      <span style={textStyle}>{text}</span>
-    </div>
+    <CategoryStyle>
+      <TextStyle>{text}</TextStyle>
+    </CategoryStyle>
   );
 };
 export default Category;
---
+++ src/components/category/Category.style.js
@@ -0,0 +1,19 @@
+import { styled } from 'styled-components';
+
+export const CategoryStyle = styled.div`
+  width: 324px;
+  height: 149px;
+  background-color: #FEA900;
+  margin: 20px; 
+  border-radius: 20px;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const TextStyle = styled.span`
+  color: #000000;
+  font-family: "Do Hyeon; sans-serif";
+  font-size: 32px;
+`;
