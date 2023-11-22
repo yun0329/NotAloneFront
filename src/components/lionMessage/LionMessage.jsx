0ae13fa0967214a9b6f00b1899fc2526181d0491
Whatdoyumin <lym4963@naver.com>
2023-11-22 22:50:46 +0900

    style: LionMessage 컴포넌트 style 코드 구분

--- src/components/lionMessage/LionMessage.jsx
+++ src/components/lionMessage/LionMessage.jsx
@@ -1,34 +1,21 @@
 import React from 'react';
+import { HeaderStyle, ButtonStyle } from './LionMessage.style';
+import { useNavigate } from 'react-router-dom';
 
 
 const Header = () => {
-  const headerStyle = {
-    width: '390px',
-    height: '172px',
-    background: 'linear-gradient(to Top, #FFFFFF 0%, #E6C88D 50%, #F2B846 75%, #F8B123 80%, #F5B433 85%, #FAAE17 90%, #FBAD10 95%, #FEA900 100%)',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: '0 20px',
-  };
-
-  const buttonStyle = {
-    background: 'none',
-    border: 'none',
-    cursor: 'pointer',
-    marginTop: '-70px',
-  };
+  const navigate = useNavigate();
 
   return (
-    <div style={headerStyle}>
-      <button style={buttonStyle}>
-        <img className = "backButton" src={"img/backButtonImage.png"} alt="뒤로가기" />
-      </button>
+    <HeaderStyle>
+      <ButtonStyle>
+        <img className = "backButton" src={"img/backButtonImage.png"} alt="뒤로가기" onClick={navigate(-1)} />
+      </ButtonStyle>
       <img className = "logo" src={"img/logo.png"} alt="로고" />
-      <button style={buttonStyle}>
-        <img className = "closeButton" src={"img/closeButton.png"} alt="닫기" />
-      </button>
-    </div>
+      <ButtonStyle>
+        <img className = "closeButton" src={"img/closeButton.png"} alt="닫기" onClick={navigate('/main')}/>
+      </ButtonStyle>
+    </HeaderStyle>
   );
 };
 
---
+++ src/components/lionMessage/LionMessage.style.js
@@ -0,0 +1,18 @@
+import { styled } from 'styled-components';
+
+export const HeaderStyle = styled.div`
+  width: 390px;
+  height: 172px;
+  background: linear-gradient(to Top, #FFFFFF 0%, #E6C88D 50%, #F2B846 75%, #F8B123 80%, #F5B433 85%, #FAAE17 90%, #FBAD10 95%, #FEA900 100%);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+`;
+
+export const ButtonStyle = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+  margin-top: -70px;
+`;
