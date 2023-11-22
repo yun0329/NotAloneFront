ed0eab1c92b0d4bfcc9c9f46ab50b6576cbf5a9c
Whatdoyumin <lym4963@naver.com>
2023-11-22 22:51:45 +0900

    style: ServiceIntro 컴포넌트 style 코드 구분

--- src/components/serviceIntro/ServiceIntro.jsx
+++ src/components/serviceIntro/ServiceIntro.jsx
@@ -1,43 +1,15 @@
 import React from 'react';
+import { ContainerStyle, ImageStyle, TextBoxStyle, TextStyle } from './ServiceIntro.style';
 
 const ImageTextBox = ({ imageSource, text }) => {
-  const containerStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    textAlign: 'center',
-  };
-
-  const imageStyle = {
-    width: 'auto',
-    height: 'auto',
-    margin: '10px',
-  };
-
-  const textBoxStyle = {
-    width: '335px',
-    height: '46px',
-    borderRadius: '30px',
-    backgroundColor: '#F0F1F3',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: '10px',
-  };
-
-  const textStyle = {
-    color: '#000000',
-    fontSize: '16px',
-    fontWeight: 'bold',
-  };
 
   return (
-    <div style={containerStyle}>
-      <img src={imageSource} alt="Image" style={imageStyle} />
-      <div style={textBoxStyle}>
-        <span style={textStyle}>{text}</span>
-      </div>
-    </div>
+    <ContainerStyle>
+      <ImageStyle src={imageSource} alt="Image" />
+      <TextBoxStyle>
+        <TextStyle>{text}</TextStyle>
+      </TextBoxStyle>
+    </ContainerStyle>
   );
 };
 
---
+++ src/components/serviceIntro/ServiceIntro.style.js
@@ -0,0 +1,31 @@
+import { styled } from 'styled-components';
+
+export const ContainerStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`;
+
+export const imageStyle = styled.img`
+  width: auto;
+  height: auto;
+  margin: 10px;
+`;
+
+export const textBoxStyle = styled.div`
+  width: 335px;
+  height: 46px;
+  border-radius: 30px;
+  background-color: #F0F1F3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+`;
+
+export const textStyle = styled.span`
+  color: #000000;
+  font-size: 16px;
+  font-weight: bold;
+`;
