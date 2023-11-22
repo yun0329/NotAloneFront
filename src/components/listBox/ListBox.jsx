fcd1a90964b1ea234fd192180b1f391f766a4612
Whatdoyumin <lym4963@naver.com>
2023-11-22 22:51:04 +0900

    style: ListBox 컴포넌트 style 코드 구분

--- src/components/listBox/ListBox.jsx
+++ src/components/listBox/ListBox.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import styled from 'styled-components';
+import { ImgStyle, Menu, MenuWrapper, TextStyle } from './ListBox.style';
 import React from 'react';
 import Toggle from '../toggle/Toggle';
 import dummy from './data.json';
@@ -25,48 +25,7 @@ const male=
         "men.png"
     ]
 }
-const Wrapper=styled.div`
-position:relative;
-left: 27%;
-display:flex;
-flex-wrap:wrap;
-gap: 15px;
-text-align: center;
-`;
-const Menu=styled.div`
-position:relative;
-width:161px;
-height:155px;
-background-color:#FEA900;
-font-size:30px;
-border-radius: 30px;
-margin-top:5px;
-vertical-align:bottom;
-`;
-const ImgStyle=styled.img`
-position:absolute;
-top:7%;
-left:20%;
-width:100px;
-height:100px;
-z-index:0;
-`;
-const TextStyle=styled.div`
-position:absolute;
-font-size:23px;
-top:70%;
-left:15%;
 
-z-index:0;
-`;
-const MenuWrapper=styled.div`
-position:relative;
-left: 27%;
-display:flex;
-flex-wrap:wrap;
-gap: 15px;
-text-align: center;
-`;
 export default function ListBox(props) {
 
 
