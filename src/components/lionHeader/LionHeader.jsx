3d9502c818a228dc9ea6770b330f4847b6435f99
Whatdoyumin <lym4963@naver.com>
2023-11-27 18:42:24 +0900

    feat: (LionMessage->) LionHeader 컴포넌트 파일명 변경

---
+++ src/components/lionHeader/LionHeader.jsx
@@ -0,0 +1,29 @@
+import React, { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { HeaderStyle, ButtonStyle } from './LionHeader.style';
+
+const Header = () => {
+  const navigate = useNavigate();
+  
+  const navigateBack = useCallback(() => {
+    navigate(-1);
+  }, [navigate]);
+
+  const navigateToMain = useCallback(() => {
+    navigate('/main');
+  }, [navigate]);
+
+  return (
+    <HeaderStyle>
+      <ButtonStyle>
+        <img className="backButton" src={"img/backButtonImage.png"} alt="뒤로가기" onClick={navigateBack} />
+      </ButtonStyle>
+      <img className="logo" src={"img/logo.png"} alt="로고" />
+      <ButtonStyle>
+        <img className="closeButton" src={"img/closeButton.png"} alt="닫기" onClick={navigateToMain} />
+      </ButtonStyle>
+    </HeaderStyle>
+  );
+};
+
+export default Header;
