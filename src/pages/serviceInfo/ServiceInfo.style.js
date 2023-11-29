7d5e04e10f8e56f0690301419ce8a78774335116
Whatdoyumin <lym4963@naver.com>
2023-11-29 17:58:59 +0900

    style: serviceInfo 스타일 설정

---
+++ src/pages/serviceInfo/ServiceInfo.style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ServiceIntroWrapper  = styled.div`
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  margin: 10px 20px;
+  align-items: center;
+  justify-content: center;
+`;
