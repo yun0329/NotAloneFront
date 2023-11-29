cdec15de3cc4075fdd43c811b3053d4e02b4d64d
Whatdoyumin <lym4963@naver.com>
2023-11-29 17:58:11 +0900

    feat: splash 페이지 컴포넌트 구조 설정

---
+++ src/pages/splash/Splash.jsx
@@ -0,0 +1,10 @@
+import LionMessage from "../../components/lionMessage/LionMessage";
+import { MainContainer } from "../serviceInfo/ServiceInfo.style";
+
+export default function SplashPage() {
+  return (
+    <MainContainer>
+      <LionMessage />
+    </MainContainer>
+  );
+}
