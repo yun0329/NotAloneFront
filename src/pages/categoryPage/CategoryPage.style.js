ae93598fba588daa7fdf4bbce7b3f9256c83c1c4
Whatdoyumin <lym4963@naver.com>
2023-11-29 18:18:41 +0900

    style: 카테고리 페이지 스타일 지정

---
+++ src/pages/categoryPage/CategoryPage.style.js
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+
+export const CategoryContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 30px 0;
+
+  strong {
+    font-size: 30px;
+    font-family: 'Pretendard-Regular';
+  }
+`;
