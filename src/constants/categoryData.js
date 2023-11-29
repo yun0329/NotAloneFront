359ed3b01dbff268afee94c72a418ad58127af8a
Whatdoyumin <lym4963@naver.com>
2023-11-29 18:20:52 +0900

    feat: 카테고리 데이터 상수값 추가

---
+++ src/constants/categoryData.js
@@ -0,0 +1,14 @@
+
+const CATEGORY_DATA = {
+  alone: {
+    name: '혼자 사는 남성/여성'
+  },
+  delivery: {
+    name: '대신해주는 배달 주문'
+  },
+  langBarrier: {
+    name: '언어 장벽 해결'
+  },
+};
+
+export default CATEGORY_DATA;
