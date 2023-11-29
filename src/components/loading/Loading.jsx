8838c69ac304f2a8b352db6d919ffdd513006e12
Whatdoyumin <lym4963@naver.com>
2023-11-29 18:21:17 +0900

    feat: Loading 컴포넌트 수정

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,11 +1,13 @@
 import Lottie from "lottie-react"; 
 import loadingLottie from "./loadingLottie.json";
+import { LionImg } from "../lionMessage/LionMessage.style";
 
 function Loading() {
   return (
-    
-        <Lottie animationData={loadingLottie} />
-      
+    <>
+      <Lottie animationData={loadingLottie} />
+      <LionImg src="img/lionBefore"/>
+    </>
   );
 }
 
