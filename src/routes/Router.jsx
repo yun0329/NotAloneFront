c9ad3cb94ae2a85369d4af75927ba9f4e8feac45
Whatdoyumin <lym4963@naver.com>
2023-11-29 17:52:49 +0900

    feat: 라우터 설정 변경 (카테고리 파라미터 추가)

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ServiceInfo from '../pages/serviceInfo/ServiceInfo';
 import Main from '../pages/main/Main';
 import Splash from '../pages/splash/Splash';
-import Category from '../pages/category/Category';
-import CustomSentence from '../pages/customSentence/CustomSentence';
+import Category from '../pages/categoryPage/CategoryPage';
 import Result from '../pages/result/Result';
 import TextInput from '../pages/textInput/TextInput';
+import CustomSentence from '../pages/customSentence/CustomSentence';
 
 
 export default function Router() {
@@ -12,9 +13,10 @@ export default function Router() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Splash />} />
+        <Route path="/serviceInfo" element={<ServiceInfo />} />
         <Route path="/main" element={<Main />} />
         <Route path="/category" element={<Category />} />
-        <Route path="/customSentence" element={<CustomSentence />} />
+        <Route path="/customSentence/:category" element={<CustomSentence />} />
         <Route path="/result" element={<Result />} />
         <Route path="/textInput" element={<TextInput />} />
       </Routes>
