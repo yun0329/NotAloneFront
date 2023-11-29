c5453f59aadb8e98bd2915a17a58c69ea184f4a3
Whatdoyumin <lym4963@naver.com>
2023-11-29 18:22:29 +0900

    feat: category 컴포넌트 수정

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { CategoryStyle, TextStyle } from './Category.style';
+import CATEGORY_DATA from '../../constants/categoryData';
 
-const Category = ({ text }) => {
+const Category = ({ category, onClick }) => {
+  const categoryInfo = CATEGORY_DATA[category];
+
+  if (!categoryInfo) {
+    return <div>카테고리 불러오는 중..</div>
+  }
+
   return (
-    <CategoryStyle>
-      <TextStyle>{text}</TextStyle>
+    <CategoryStyle onClick={() => onClick(category)}>
+      <TextStyle>{categoryInfo.name}</TextStyle>
     </CategoryStyle>
   );
 };
+
 export default Category;
