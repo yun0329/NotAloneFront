e6238260a500779285fa0e1a215b5395ccf89908
Whatdoyumin <lym4963@naver.com>
2023-12-07 00:37:26 +0900

    feat: main페이지 전역변수 설정

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,39 +1,15 @@
 import React, { useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '../../components/button/Button';
-import LionMessage from '../../components/lionHeader/LionHeader'
+import LionMessage from '../../components/lionHeader/LionHeader';
 import { MainContainer } from '../serviceInfo/ServiceInfo.style';
-import { QuickContainer, QuickHeader, UserBtnContainer,Menu,MenuWrapper,ImgStyle,TextStyle} from './Main.style';
-// import dummy from '../src/components/listBox/data.json';
-const female={
-  "images":[
-      "women.png",
-      "women3.png",
-      "women.png",
-      "women.png",
-  ]
-}
-const dummy={
-  "sentences":[
-    {"id":19, 
-        "category":"main",
-        "content": "누구세요?"},
-        {"id":20, 
-        "category":"main",
-        "content": "필요없어요"},
-        {"id":21, 
-        "category":"main",
-        "content": "문 앞에 두세요"},
-        {"id":22, 
-        "category":"main",
-        "content": "여기 아니에요"}
-  ]
-
-}
+import { QuickContainer, QuickHeader, UserBtnContainer, Menu, MenuWrapper, ImgStyle, TextStyle } from './Main.style';
+import CATEGORY_DATA from '../../constants/categoryData'; 
 
 export default function Main() {
   const navigate = useNavigate();
-  const menuList = dummy.sentences.filter(sentence => sentence.category ==="main");
+  const mainCategory = CATEGORY_DATA.alone;
+  const menuList = mainCategory.list.slice(0, 4);
 
   const navigateToCategory = useCallback(() => {
     navigate('/category');
@@ -52,14 +28,14 @@ export default function Main() {
           <p>자주 사용하는 문장들</p>
         </QuickHeader>
         <MenuWrapper>
-        {menuList.map(sentence=>{
-        return (
-        <Menu>
-            <ImgStyle src={`img/${female}.images[0]`} />
-            <TextStyle>{sentence.content}</TextStyle>
-        </Menu>
-        );    
-        })}
+          {menuList.map((sentence) => {
+            return (
+              <Menu key={sentence.id}>
+                <ImgStyle src={sentence.img.w} />
+                <TextStyle>{sentence.content}</TextStyle>
+              </Menu>
+            );
+          })}
         </MenuWrapper>
       </QuickContainer>
       <UserBtnContainer>
