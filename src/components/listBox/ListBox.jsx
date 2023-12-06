05ef8945c70e6dc7e4ec6adc4c60236dbfce9687
Whatdoyumin <lym4963@naver.com>
2023-12-07 00:35:52 +0900

    feat: ListBox컴포넌트 전역변수 사용

--- src/components/listBox/ListBox.jsx
+++ src/components/listBox/ListBox.jsx
@@ -1,74 +1,38 @@
-import { useState} from 'react';
+import { useState } from 'react';
 import { ToggleContainer, Span1, Span2 } from './Toggle.style';
 import { ImgStyle, Menu, MenuWrapper, TextStyle, Wrapper } from './ListBox.style';
 import React from 'react';
-import dummy from './data.json';
-//import axios from 'axios';
-const female={
-    "images":[
-        "women.png",
-        "women3.png",
-        "women.png",
-        "women.png",
-        "women.png",
-        "women.png"
-    ]
-}
-const male=
-{
-    "images":[
-        "men.png",
-        "men.png",
-        "men.png",
-        "men.png",
-        "men.png",
-        "men.png"
-    ]
-}
+import CATEGORY_DATA from '../../constants/categoryData'; // Import the category data
+
 const ListBox = (props) => {
   const [isOn, setisOn] = useState(false);
 
-  const menuList = dummy.sentences.filter(sentence => sentence.category ===props.category);
+  const menuList = CATEGORY_DATA[props.category].list;
   const toggleHandler = () => {
-    setisOn(!isOn)
-    console.log({isOn})
+    setisOn(!isOn);
+    console.log({ isOn });
   };
-  console.log(menuList);
 
-  //통신
-  // const playAudio=(e)=>{
-  //   axios.get(`https://imnotalone.shop:8090/voice/get?prompt=${e.value}&gender=${isOn === false ? male:female}`)
-  //   .then(function (response) {
-  //        const audioData=response
-  //        return(
-  //         <audio src={audioData}></audio>
-  //        );
-  //   }).catch(function (error) {
-  //       console.log(error);
-  //   });
-
-  // };
   return (
     <Wrapper>
-      <ToggleContainer
-        onClick={toggleHandler}
-      >
-        <div className={`toggle-container ${isOn ? "toggle--checked" : null}`}/>
+      <ToggleContainer onClick={toggleHandler}>
+        <div className={`toggle-container ${isOn ? "toggle--checked" : null}`} />
         <Span1>여자</Span1>
         <Span2>남자</Span2>
-        <div className={`toggle-circle ${isOn ? "toggle--checked" : null}`}/>
+        <div className={`toggle-circle ${isOn ? "toggle--checked" : null}`} />
       </ToggleContainer>
-      {/*off = 남자, on= 여자 */}
       <MenuWrapper>
-        {menuList.map(sentence=>{
-        return (
-        // <Menu onClick={(e)=>playAudio(e)}>
-        <Menu>
-            <ImgStyle src={`img/${isOn === false ? male.images[0] : female.images[0]}`} />
-            <TextStyle>{sentence.content}</TextStyle>
-        </Menu>
-        );    
-      })}
+        {menuList.map((sentence) => {
+          const images = sentence.img;
+          const selectedImage = isOn ? images?.w : images?.m;
+
+          return (
+            <Menu key={sentence.id}>
+              <ImgStyle src={selectedImage} />
+              <TextStyle>{sentence.content}</TextStyle>
+            </Menu>
+          );
+        })}
       </MenuWrapper>
     </Wrapper>
   );
