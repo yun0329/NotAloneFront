1ab9e7f91b0c7530730a0ebe39055a37c52aa8b0
Whatdoyumin <lym4963@naver.com>
2023-12-06 17:17:30 +0900

    feat: Header 컴포넌트 수정

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { HeaderStyle, ButtonStyle } from './Header.style';
 
 const Header = () => {
+  const navigate = useNavigate();
+ 
+  const navigateBack = useCallback(() => {
+    navigate(-1);
+  }, [navigate]);
+
+  const navigateToMain = useCallback(() => {
+    navigate('/main');
+  }, [navigate]);
+
   return (
     <HeaderStyle>
       <ButtonStyle>
-        <img className = "backButton" src={"img/backButtonImage.png"} alt="뒤로가기" />
+        <img className = "backButton" src={"img/backButtonImage.png"} alt="뒤로가기" onClick={navigateBack} />
       </ButtonStyle>
   
       <ButtonStyle>
-        <img className = "closeButton" src={"img/closeButton.png"} alt="닫기" />
+        <img className = "closeButton" src={"img/closeButton.png"} alt="닫기" onClick={navigateToMain} />
       </ButtonStyle>
     </HeaderStyle>
   );
